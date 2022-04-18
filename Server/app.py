27afe87eae16cecffa591244642b4707419be6cf
FkerYJ <836703704@qq.com>
2022-04-18 23:28:33 +0800

    2

--- Server/app.py
+++ Server/app.py
@@ -1,3 +1,5 @@
 from flask import Flask,request,redirect,make_response,render_template
 from flask import send_from_directory,send_file  
 import os.path as path
+
+app = Flask(__name__,template_folder='')
