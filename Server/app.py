c664640702888c30e697a5b856a37e7091e4a287
FkerYJ <836703704@qq.com>
2022-04-18 22:46:28 +0800

    1

--- Server/app.py
+++ Server/app.py
@@ -1,2 +1,3 @@
-from flask import send_from_directory
+from flask import Flask,request,redirect,make_response,render_template
+from flask import send_from_directory,send_file  
 import os.path as path
