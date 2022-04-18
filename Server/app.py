38c424c1b61e2dccb6c53b48507433e9f62b4e7e
FkerYJ <836703704@qq.com>
2022-04-18 23:30:29 +0800

    git

--- Server/app.py
+++ Server/app.py
@@ -1,5 +1,10 @@
 from flask import Flask,request,redirect,make_response,render_template
 from flask import send_from_directory,send_file  
 import os.path as path
+import time,random,sys
+from datetime import datetime,timedelta
 
 app = Flask(__name__,template_folder='')
+
+if __name__ == '__main__':
+    app.run(debug=True,port=19268,host="0.0.0.0")
