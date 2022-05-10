a610a27e9989cf3391fe890041b043bad1fdbd34
fkeryj <836703704@qq.com>
2022-05-10 10:43:59 +0800

    update annotation

--- CourseMgmt/homework.py
+++ CourseMgmt/homework.py
@@ -4,23 +4,48 @@ import Utils.compress as cmpr
 
 ###########task_mgmt###########
 def list_crouse_task(courseId):
+  ret={
+   "1":{"name":"第一章习题","taskId":234,} ,
+   "2":{"name":"第二章习题","taskId":316,} ,
+   "3":{"name":"第二章习题(2)","taskId":536,}, 
+  }
   pass
 
-def create_course_task(courseId):
+def list_user_task(userId):
+  ret= {
+   "1":{"name":"第一章习题", "courseName":"计算机网络", "courseId":24,"taskId":234,} ,
+   "2":{"name":"第一周", "courseName":"编译原理", "courseId":12,"taskId":231,} ,
+  }
+  return
+
+def create_course_task(courseId,data):
+  '''parameter data will be a dict,contain key
+  name, des, attentionId,Deadline
+  the func return taksId
+  '''
   pass
 
-def view_task(courseId):
-  pass
+def view_task(taskId):
+  '''return a dict contain key same as "create_task" '''
+  return
 
 ######hmwk_mgmt############
-def list_class_hmwk(classId):
-  return
 
 def view_hmwk(hmwkId):
+  '''ret a dict contain all hmwk version '''
   return
 
-def submit_hmwk(hmwkId):
+def rollback_hmwk(hmwkId,version):
+  """reset hmwk version to old ret 1 when suc
+  return 0 when creates failed because of noexist id"""
+  return 
+
+def submit_hmwk(taskId,userId,data):
+  """generate and ret a hmwkId for the taskId of user"""
+  """return -1 when creates failed because of insuffcient data or wrong userId/taskId"""
   return
 
-def update_hmwk(hmwkId):
+def update_hmwk(hmwkId,data):
+  '''return now versionId'''
+  """return -1 when update failed because of insuffcient data"""
   return
--- CourseMgmt/material.py
+++ CourseMgmt/material.py
@@ -3,24 +3,34 @@ sys.path.append(path.dirname(__file__))
 from datetime import datetime,timedelta
 from Utils.DataFrame import *
 from Utils.Database import conn
+
+"""The implementation principle of this module 
+    is highly similar to hmwt
+    you could refer to detail annotation in homework.py
+"""
 ################course_material##################
 def list_course_material(classId):
   return
 
 def view_material(materialId):
-  """return the newest version and simple inform of history version"""
+  """return all version """
   return
 
 def rollback_course_material(materialId,version):
   """恢复到历史版本"""
   return
 
-def create_course_material(classId):
-  """return -1 when creates failed"""
+def create_course_material(classId,data):
+  data={
+    "title":"课件1",
+    "descript":"the ppt of the first chapter",
+    "attachId":2435,
+  }
   materialId =3
   return materialId
 
-def update_course_material(materialId):
+def update_course_material(materialId,data):
+  "refer to the descript of update_hmwk"
   version+=1
   return
 
