7413d99e4d010b76c4bf7239e0f598eb9417ebd3
fkeryj <836703704@qq.com>
2022-06-16 00:08:57 +0800

    normal sync

--- install.py
+++ install.py
@@ -4,8 +4,8 @@ from Utils.database import conn
 from Utils import user
 usertb=conn.create("user")
 usertb.set_ukey("id")
-user.sign_up("2020211838", "12345678", "教师", "珠峰", "17623596408")
-user.sign_up("2020211839", "12345678", "学生", "彭木", "17623596409")
+user.sign_up("2020211838", "12345678", "教师", "珠峰", "17623596408",23)
+user.sign_up("2020211839", "12345678", "学生", "彭木", "17623596409",16)
 
 
 #file初始数据
