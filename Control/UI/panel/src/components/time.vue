0be9701fce4b06cdf77d1d05ee1dd3eb9bb0e92d
fkeryj <836703704@qq.com>
2022-06-15 08:19:34 +0800

    ui

--- Control/UI/panel/src/components/time.vue
+++ Control/UI/panel/src/components/time.vue
@@ -12,25 +12,29 @@
     <el-sub-menu index="1">
       <template #title>
         <el-icon><location /></el-icon>
-        <span>学业</span>
+        <span>Navigator One</span>
       </template>
-        <el-menu-item index="1-1" @click="viewclass">课程表</el-menu-item>
-        <el-menu-item index="1-2" @click="viewmetal">资料</el-menu-item>
-        <el-menu-item index="1-3" @click="viewhmwk">作业</el-menu-item>
-        <!-- <el-sub-menu index="1-3" @click="viewexam">子菜单</el-sub-menu> -->
-        <el-menu-item index="1-4" @click="viewhmwk">考试</el-menu-item>
+      <el-menu-item-group title="Group One">
+        <el-menu-item index="1-1-1">item one</el-menu-item>
+        <el-menu-item index="1-1-2">item two</el-menu-item>
+        <el-menu-item index="1-2">item three</el-menu-item>
+      </el-menu-item-group>
+      <el-sub-menu index="1-4">
+        <template #title><span>item four</span></template>
+        <el-menu-item index="1-4-1">1-4-1</el-menu-item>
+      </el-sub-menu>
     </el-sub-menu>
     <el-menu-item index="2">
       <el-icon><icon-menu /></el-icon>
-      <template #title>活动管理</template>
+      <template #title>Navigator Two</template>
     </el-menu-item>
     <el-menu-item index="3">
       <el-icon><document /></el-icon>
-      <template #title>日程提醒</template>
+      <template #title>Navigator Three</template>
     </el-menu-item>
     <el-menu-item index="4">
       <el-icon><setting /></el-icon>
-      <template #title>个人设置</template>
+      <template #title>Navigator Four</template>
     </el-menu-item>
   </el-menu>
 </el-aside>
@@ -39,13 +43,9 @@
 
 <el-main>
    <el-table :data="filterTableData"  :key="refreshNum" style="width: 100%">
-      <el-table-column
-        v-for="(items,indexs) in tableHead"
-        :key="indexs"
-        :min-width="items.prop==='industryName'||items.prop==='investmentName'?100:150"
-        :prop="items.prop"
-        :label="items.label"
-      ></el-table-column>
+    <el-table-column label="名称" prop="name" />
+    <el-table-column label="星期" prop="day" />
+    <el-table-column label="教授" prop="speaker" />
     <el-table-column align="right">
       <template #header>
         <el-input v-model="search" size="small" placeholder="Type to search" />
@@ -85,7 +85,6 @@ import cookies from 'vue-cookies'
 const session = cookies.get("session")
 console.log(session)
 
-
 interface User {
   name: string
   day: string
@@ -100,7 +99,6 @@ const handleDelete = (index: number, row: User) => {
 }
 
 const isCollapse = ref(true)
-
 const handleOpen = (key: string, keyPath: string[]) => {
   console.log(key, keyPath)
 }
@@ -139,46 +137,30 @@ const tableData: User[] = [
   },
 ]
 
-const tableHead=[
-      {label: '名称', prop: 'name'},
-      {label: '星期', prop: 'day'},
-      {label: '教授', prop: 'speaker'},
-  ]
-
-const viewmetal=()=>{
-  tableHead.length=0;
+axios.post("http://192.168.1.88:1024/api/classlist",
+{
+  session:session,
+})//传参
+.then((res: any)=>{
+if(res.data.code==1)
+{
+    console.log(res.data.mess)
+    ElMessage({
+      type: 'info',
+      message: `提示: 登录成功`,
+    })
+    // this.$router.push({name:'home',params: {id:'10001'}})
 }
-const viewhmwk=()=>{
-  tableHead.length=0;
-  tableHead.push({label:"课程",prop:"className"})
+else{
+  throw res.data.mess
 }
-const viewclass=()=>{
-  axios.post("http://192.168.1.88:1024/api/classlist",
-  {
-    session:session,
-  })//传参
-  .then((res: any)=>{
-  if(res.data.code==1)
-  {
-      console.log(res.data.mess)
-      ElMessage({
-        type: 'info',
-        message: `提示: 登录成功`,
-      })
-      // this.$router.push({name:'home',params: {id:'10001'}})
-  }
-  else{
-    throw res.data.mess
-  }
-  })
-  .catch(function(err: any){
-      ElMessage({
-        type: 'info',
-        message: `提示: ${err}`,
-      })
-  });
-}
-
+})
+.catch(function(err: any){
+    ElMessage({
+      type: 'info',
+      message: `提示: ${err}`,
+    })
+});
 
 const addTodo = (user: User): void => {
   tableData.push(user)
