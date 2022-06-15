7706063b23820a0bbdfa8e057f5f519760777aeb
fkeryj <836703704@qq.com>
2022-06-15 18:27:32 +0800

    rewrite Log

--- Utils/Log.py
+++ Utils/Log.py
@@ -3,20 +3,24 @@ sys.path.append(path.dirname(__file__))
 import DataFrame,Time
 absdir=path.dirname(__file__)
 logdir=path.dirname(absdir)+"/Control/log/"
-actPwd=logdir+"action.log"
-infoPwd=logdir+"info.log"
-warnPwd=logdir+"warn.log"
-errorPwd=logdir+"error.log"
+actPwd=logdir+"action.log" #用户操作
+warnPwd=logdir+"warn.log" #用户警告
+infoPwd=logdir+"info.log" #管理员通知信息/轻度错误
+errorPwd=logdir+"error.log" #系统致命报错
 
 
-def log(mess,level=1):
+def log(mess,level=0):
   if not path.exists(logdir): os.mkdir(logdir)
   """
   0=act 1=info 2=warn 3=error
   """
-  if level==0: fw=open(actPwd,"a+")
-  if level==1: fw=open(infoPwd,"a+")
-  if level==2: fw=open(warnPwd,"a+")
-  if level==3: fw=open(errorPwd,"a+")
+  if level==0: fw=open(actPwd,"a+",encoding="utf-8")
+  if level==1: fw=open(warnPwd,"a+",encoding="utf-8")
+  if level==2: fw=open(infoPwd,"a+",encoding="utf-8")
+  if level==3: fw=open(errorPwd,"a+",encoding="utf-8")
   fw.write(str(Time.now())+" : "+mess+"\n")
   fw.close()
+
+
+if __name__ == '__main__':
+  log("Time module test 你好",level=2)
