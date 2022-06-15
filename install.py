38f31e5d3d885050cdf71620fd8dce9d1d78e945
fkeryj <836703704@qq.com>
2022-06-15 19:41:48 +0800

    rename

--- install.py
+++ install.py
@@ -1,7 +1,10 @@
 from Utils.database import conn
 
 #user初始数据
-conn.create("user")
+from Utils import user
+usertb=conn.create("user")
+user.sign_up("2020211838", "12345678", "教师", "珠峰", "17623596408")
+user.sign_up("2020211839", "12345678", "学生", "彭木", "17623596409")
 
 
 #file初始数据
