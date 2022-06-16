4d02aed2e2096f03efa0f6abf2077f83f047e0a2
fkeryj <836703704@qq.com>
2022-06-16 10:11:19 +0800

    ui

--- Control/UI/panel/src/components/home.vue
+++ Control/UI/panel/src/components/home.vue
@@ -129,11 +129,7 @@ export default defineComponent({
           {
             id: '0',
             名称: '军事理论',
-            星期: '星期一',
-            节次: '3',
-            时长: '1',
-            教师: '周光',
-            地点: 3,
+
           },
         ],
         _tableHead:[
@@ -193,10 +189,7 @@ export default defineComponent({
     },
     listclass(){
       axios.post("/api/crouse/list",
-      // axios.post("/api/signin",
       {
-        // "act":"username",
-        // "pwd":"password",
         "session":this.session
       })//传参
       .then((res: any)=>{
@@ -212,11 +205,7 @@ export default defineComponent({
             this._tableHead.push({label:key,prop:key})
 
           }
-          for(item in data){
-            console.log(item)
-            this.tableData.push(item)
-          }
-
+          this.tableData=data
         }
         else if(res.data.code==-1){
            ElMessage({
@@ -240,26 +229,49 @@ export default defineComponent({
 
     listhmwk(){
       this._tableHead.length=0;
-      this._tableHead.pop()
-      this._tableHead.pop()
-      this._tableHead.push({label:"课程",prop:"className"})
       this._tableHead.push({label:"截止时间",prop:"ddl"})
     },
     listexam(){
-      this._tableHead.length=0;
-      this._tableHead.pop()
-      this._tableHead.pop()
-      this._tableHead.push({label:"课程",prop:"className"})
-      this._tableHead.push({label:"截止时间",prop:"ddl"})
+      axios.post("/api/crouse/list",
+      {
+        "session":this.session
+      })//传参
+      .then((res: any)=>{
+        if(res.data.code==1)
+        {
+          let data=res.data.mess;
+          if(data.length==0) return;
+          let item:any=data[0];
+
+          this._tableHead.length=0;
+          for(var key in item){
+            console.log(key,item[key])
+            this._tableHead.push({label:key,prop:key})
+
+          }
+          this.tableData=data
+        }
+        else if(res.data.code==-1){
+           ElMessage({
+              type: 'info',
+              message: `提示: 登录失效`,
+           })
+           this.$router.push({name:'login',params: {id:'10001'}})
+        }
+        else{
+          console.log(res.data.code)
+          throw res.data.mess
+        }
+      })
+      .catch(function(err: any){
+           ElMessage({
+              type: 'info',
+              message: `提示: ${err}`,
+           })
+      });
     },
     listmetal(){
-      // this._tableHead.length=0;
-      const user:any={
-        name: '计算机系统基础',
-        day: '星期一',
-        speaker: '周峰',
-      }
-      this.tableData.push(user)
+      this._tableHead.length=0;
     },
   }
    
