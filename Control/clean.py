0f304dd3bf990f2b9fc71533c420487b12c0b635
fkeryj <836703704@qq.com>
2022-06-19 11:40:38 +0800

    cfg.lock enhance

--- Control/clean.py
+++ Control/clean.py
@@ -3,10 +3,10 @@ import os,sys,time,os.path as path
 pwd=path.dirname(path.dirname(__file__))
 pjoin=path.join
 try:
-  shutil.rmtree(pjoin(pwd,"DataBase"))
+  shutil.rmtree(pjoin(pwd,"Database"))
   os.remove(pjoin(pwd,"cfg.json"))
   shutil.rmtree(pjoin(pwd,"Upload"))
-except:pass
+except Exception as e: print(e)
 
 try:
   os.makedirs(pjoin(pwd,"Upload/tmp"))
--- start.py
+++ start.py
@@ -1,5 +1,7 @@
 # from pytest import importorskip
 from xmlrpc.client import Server
 from Control.rpc.app import app
+import os
 
+os.remove("Control/cfg.lock")
 app.run(debug=True,port=1024,host="0.0.0.0",threaded=True)
